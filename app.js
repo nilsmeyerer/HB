16315c6903081ba6b5e43d3e1164de0a60d18039
Nils <102951919+nilsmeyerer@users.noreply.github.com>
2022-10-13 17:23:31 +0200

    Delete app.js

--- app.js
+++
@@ -1,2 +0,0 @@
-
-(function(e,t,n,r,i,s,o){e[i]=e[i]||function(){(e[i].q=e[i].q||[]).push(arguments)}, e[i].l=1*new Date;s=t.createElement(n),o=t.getElementsByTagName(n)[0];s.async=1; s.src=r;o.parentNode.insertBefore(s,o)})(window,document,"script", 'https://www.handball.net/widgets/embed/v1.js',"_hb");
